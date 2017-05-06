4177e9439e053b6966c873338a2e7efdfdc49bf4
Ross Flieger-Allison <rfliegerallison@gmail.com>
2017-05-06 14:34:14 -0400

    Adds comments to GameObject

--- GameObject.java
+++ GameObject.java
@@ -2,26 +2,40 @@ import java.awt.Graphics;
 
 public class GameObject {
 
-    // x and y coordinates of the GameObject
+    // Spatial coordinates of the object within the game environment.
     private int x, y;
 
+    // The default diameter of the object (which, by default, has an
+    // oval shape).
     private final int DEFAULT_DIAMETER = 10;
 
-    // Initialize x and y coordinates of the game object
+    // Initialization.
     public GameObject(int x, int y) {
         this.x = x;
         this.y = y;
     }
-    
-    // Getter and Setter for x
-    public int getX() { return x; }
-    public void setX(int x) { this.x = x; }
 
-    // Getter and Setter for y
-    public int getY() {	return y; }
-    public void setY(int y) { this.y = y; }
+    // Gets the object's x-coordinate.
+    public int getX() {
+        return x;
+    }
 
-    // Method to be overridden depending on the gameobject
+    // Sets the object's x-coordinate.
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    // Gets the object's y-coordinate.
+    public int getY() {
+        return y;
+    }
+
+    // Sets the object's y-coordinate.
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    // A default draw method, to be overridden.
     public void draw(Graphics g) {
         g.drawOval(x, y, DEFAULT_DIAMETER, DEFAULT_DIAMETER);
     }
